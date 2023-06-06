29ce664994ffec9a53dc0f990d45ee83b1189ee1
irohacoding <info@irohacoding.com>
2023-06-06 14:20:34 +0900

    Update qk-mode.el
    * Changed display favorite word to coffee cup with steam :D

--- qk-mode.el
+++ qk-mode.el
@@ -5,17 +5,35 @@
 ;; Author: IrohaCoding <info@irohacoding.com>
 ;; Keywords: tools
 
-;; Version: 0.1.0
+;; Version: 0.1.1
 ;; Package-Requires: ((emacs "27.1"))
 ;; URL: https://github.com/irohacoding/qk-mode
+
+;; This file is not part of GNU Emacs, but is distributed under
+;; the same terms.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either verion 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs. If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
 ;; QK Mode is to take a break for hard working users.
+;; Type M-x qk for start qk-mode. Type C-g to stop steam
+;; and type C-g again to exit (kill *break* buffer). 
 
 ;;; Code:
 
-(defconst qk-mode-version "0.1.0"
+(defconst qk-mode-version "0.1.1"
   "QK Mode version.")
 
 (defgroup qk-mode nil
@@ -24,27 +42,69 @@
   :prefix "qk-"
   :link '(url-link :tag "Repository" "https://github.com/irohacoding/qk-mode"))
 
-(defcustom qk-favorite-word "\"Time flies like an arrow; fruit flies like a banana.\" - Groucho Marx"
-  "Display favorite word in *break* buffer.
-
-Change your best favorite word."
-  :type 'string
-  :group 'qk-mode)
-
 (defvar qk-mode-buffer "*break*"
   "Buffer name for qk-mode")
 
+(defvar qk-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-g") 'qk-mode-finish)
+    map))
+
 (define-derived-mode qk-mode text-mode "QK"
-  "Major mode for taking a break.")
+  "Major mode for taking a break."
+  (setq cursor-type nil))
 
 (defun qk ()
   "Take a break!"
   (interactive)
-  (if (get-buffer qk-mode-buffer)
-      (switch-to-buffer qk-mode-buffer)
-    (switch-to-buffer (get-buffer-create qk-mode-buffer))
-    (qk-mode)
-    (insert qk-favorite-word)))
+  (switch-to-buffer (get-buffer-create qk-mode-buffer))
+  (qk-mode)
+  (qk-coffee-break))
+
+(defun qk-coffee-break ()
+  "Display coffee cup and steam with animation."
+  (let ((cup-parts '("________\n"
+                     "  |        |-_\n"
+                     "   |        | ||\n"
+                     "  |________|_-\n"
+                     "\\______/")))
+    (save-excursion
+      (insert (make-string 16 ?\n))
+      (dolist (cup cup-parts)
+        (qk-insert-center cup))
+      (goto-line (- (line-number-at-pos) 5))
+      (qk-insert-steam))))
+
+(defun qk-insert-steam ()
+  "Insert and repeat steam like animation."
+  (let ((steam-parts '("\\ | /"
+                       "/ / \\"
+                       " ` \\"
+                       " . /"
+                       "   `,")))
+    (while t
+      (dolist (steam steam-parts)
+        (sit-for 1)
+        (qk-insert-center steam)
+        (goto-line (- (line-number-at-pos) 1)))
+      (goto-line (+ (line-number-at-pos) 5))
+      (dotimes (i 5)
+        (sit-for 1)
+        (delete-line)
+        (insert "\n")
+        (goto-line (- (line-number-at-pos) 2)))
+      (goto-line (+ (line-number-at-pos) 5)))))
+
+(defun qk-insert-center (text)
+  "Insert text centered in the current buffer."
+  (let ((width (- (frame-width) 1)))
+    (insert (make-string (/ (- width (length text)) 2) ?\s))
+    (insert text)))
+
+(defun qk-mode-finish ()
+  "Kill *break* buffer for finish qk-mode."
+  (interactive)
+  (kill-buffer qk-mode-buffer))
 
 (provide 'qk-mode)
 
